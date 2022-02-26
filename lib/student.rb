dcea9a309d3c3b286fbdafe38f3deb1a095f75b2
Daniel Rozmajzl <daniel.rozmajzl@gmail.com>
2022-02-26 15:35:13 -0500

    commit tree 24e1ffeddedd1fde32b87f21d54ec41849374c98

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require 'pry'
+class Student < User
+    attr_accessor :knowledge
+
+    def initialize()
+    @knowledge = Array.new()
+    end
+
+    def learn(s)
+        @knowledge << s 
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
